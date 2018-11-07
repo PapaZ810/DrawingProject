003c873f5a4d9930b11a92f473124babe5b3b4cd
Zac <zombiezac@att.net>
2018-11-07 13:36:15 -0700

    connected the code to the main view

---
+++ DrawingProject/View/DrawingCell.swift
@@ -0,0 +1,15 @@
+//
+//  DrawingCell.swift
+//  DrawingProject
+//
+//  Created by Durham, Zachary on 11/7/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class DrawingCell: UICollectionViewCell
+{
+    @IBOutlet weak var drawingImage: UIImageView!
+    @IBOutlet weak var drawingLabel: UILabel!
+}
